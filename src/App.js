3af3642857b497aae0444295ccebf31e2dc2dd26
Andreea <zleiandreea@gmail.com>
2018-11-17 14:30:11 +0200

    Added User Profile page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.scss';
 
 import Header from './components/header/Header.component';
-import BloodRectangle from './components/BloodRectangle/BloodRectangle.component'
-import './App.css';
+import UserProfile from './components/userProfile/UserProfile.component'
+import BloodRectangle from './components/bloodRectangle/BloodRectangle.component'
+import './App.scss';
 import Auth from './components/auth/Auth.component';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Header appName = {"Trei Vieti"}/>
-        <BloodRectangle bloodType="A" percentage="22" />
-        <BloodRectangle bloodType="B" percentage="55" />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Bla bla <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <div className="container">
-          <Header appName = {"Trei Vieti #2"} />
-          <Auth />
+      <div className="App">        
+        
+        <div className="container">          
+          <UserProfile />
         </div>
       </div>
     );
--- src/components/BloodRectangle/BloodRectangle.component.jsx
+++ src/components/BloodRectangle/BloodRectangle.component.jsx
@@ -9,7 +9,7 @@ class BloodRectangle extends React.Component {
     render() {
         return (
             <div className="blood-rectangle" style={{background:`linear-gradient(to top, red 0%,red ${this.props.percentage}%,white ${this.props.percentage}%,white 100%)`}}>
-                <p> {this.props.bloodType} </p>
+                <h1> {this.props.bloodType} </h1>
                 <p> {this.props.percentage} % </p>
 
             </div>
