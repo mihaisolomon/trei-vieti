f455ed346012e21b6145f187bd000d26446346d0
Daniel\dll <daniel@crosstechit.com>
2018-11-17 14:36:15 +0200

    add notifications view

---
+++ src/components/NotificationsView/data.js
@@ -0,0 +1,71 @@
+export const offlineNotifications = [
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld ",
+      'created_at' : new Date(),
+      'read': false,
+    },
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld www.treivieti.ro ",
+      'created_at' : new Date(),
+      'read': false,
+    },
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld ",
+      'created_at' : new Date(),
+      'read': false,
+    },
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld ",
+      'created_at' : new Date(),
+      'read': false,
+    },
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld ",
+      'created_at' : new Date(),
+      'read': false,
+    },
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld ",
+      'created_at' : new Date(),
+      'read': false,
+    },
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld ",
+      'created_at' : new Date(),
+      'read': false,
+    },
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld ",
+      'created_at' : new Date(),
+      'read': false,
+    },
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld ",
+      'created_at' : new Date(),
+      'read': false,
+    },
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld ",
+      'created_at' : new Date(),
+      'read': false,
+    },
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld ",
+      'created_at' : new Date(),
+      'read': false,
+    },
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld ",
+      'created_at' : new Date(),
+      'read': false,
+    },
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld ",
+      'created_at' : new Date(),
+      'read': false,
+    },
+    {
+      'body' : "Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld Lorem ipsum toll asld ",
+      'created_at' : new Date(),
+      'read': false,
+    }];
